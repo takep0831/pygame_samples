c94ab50afb5f321882e93c40d87d68f809a49f4b
takep0831 <195740480+takep0831@users.noreply.github.com>
2025-03-02 06:53:48 +0900

    k

--- demo_01.py
+++ demo_01.py
@@ -19,7 +19,7 @@ while running:
 
     screen.fill((255, 255, 0))  # back ground color
 
-    pygame.draw.circle(screen, (176, 176, 222), (300, 200), 120)
+    pygame.draw.circle(screen, (176, 176, 222), (500, 300), 120)
     pygame.draw.circle(screen, (222, 176, 222), (120, 120), 20)
     pygame.draw.circle(screen, (222, 176, 222), (120, 120), 20)
     pygame.draw.rect(screen, (120, 120, 120), Rect(120, 120, 200, 120))
