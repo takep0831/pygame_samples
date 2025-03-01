c10decddc1d6abe6f85486afa20f2c8a9a539f4e
takep0831 <195740480+takep0831@users.noreply.github.com>
2025-03-01 21:48:31 +0900

    大きさテスト

--- demo_01.py
+++ demo_01.py
@@ -4,7 +4,7 @@ from pygame.locals import Rect
 pygame.init()
 
 clock = pygame.time.Clock()
-screen = pygame.display.set_mode([640, 480])
+screen = pygame.display.set_mode([320, 240])
 pygame.display.set_caption("pygame demo - window title here")
 
 running = True
