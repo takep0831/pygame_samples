271a7452f9bfaf829704efbc05793246b25e8629
takep0831 <195740480+takep0831@users.noreply.github.com>
2025-05-31 20:42:41 +0900

    a

--- demo_03.py
+++ demo_03.py
@@ -54,11 +54,11 @@ while running:
         display1.disp_num2(zfil=True, rjust=8, num=time_now, base=10)
 
         dt_now = datetime.now()
-        time_now = (dt_now.year * 10 ** 5
+        time_now = (dt_now.year * 10 ** 6
                     + dt_now.month * 10 ** 3
                     + dt_now.day * 1)
 
-        display2.disp_num2(zfil=True, rjust=10, num=time_now, base=10)
+        display2.disp_num2(zfil=True, rjust=9, num=time_now, base=10)
 
         dt_now = datetime.now()
         time_now = (dt_now.hour * 10000
