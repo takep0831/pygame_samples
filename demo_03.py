c4e6332a32831e4249f9e01fee6356f7fd2bcb82
takep0831 <195740480+takep0831@users.noreply.github.com>
2025-05-31 19:39:39 +0900

    a

--- demo_03.py
+++ demo_03.py
@@ -54,9 +54,9 @@ while running:
             break
         # 「for count」のループから抜ける。whileループも抜ける。
 
-        display1.update_col(col=0, num=count // (50 ** 3))   # 4096の位
-        display1.update_col(col=1, num=count // (16 ** 2))   # 256の位
-        display1.update_col(col=2, num=count // 16)          # 16の位
+        display1.update_col(col=0, num=count // 4)   # 4096の位
+        display1.update_col(col=1, num=count // 3)   # 256の位
+        display1.update_col(col=2, num=count // 2)          # 16の位
         display1.update_col(col=3, num=count)                # 1の位
 
         display2.update_col(col=0, num=count // (10 ** 4), base=10)   # 1000の位
