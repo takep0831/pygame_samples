41838f888c1825dc8de7e5a5333d191de0d7ba60
takep0831 <195740480+takep0831@users.noreply.github.com>
2025-05-31 19:50:55 +0900

    a

--- demo_03.py
+++ demo_03.py
@@ -58,7 +58,7 @@ while running:
         display2.update_col(col=4, num=count // (10 ** 0), base=10)   # 1の位
 
         dt_now = datetime.now()
-        time_now = (dt_now.hour * 60
+        time_now = (dt_now.hour * 0
                     + dt_now.minute * 60
                     + dt_now.second)
         display3.disp_num2(zfil=True, rjust=6, num=time_now, base=10)
