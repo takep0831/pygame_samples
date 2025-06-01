6d2dc93f578cbc39ba2371874474c4aa32d452e8
takep0831 <195740480+takep0831@users.noreply.github.com>
2025-06-01 18:00:45 +0900

    a

--- demo_03.py
+++ demo_03.py
@@ -28,7 +28,7 @@ display1.init_row(X_ORG=5, Y_ORG=50, COL_INTV=8)
 
 display2 = Seven_seg(screen)
 display2.init_col(BLOCK_SIZE=7, BLOCK_INTV=8, COLOR_ON=RED, COLOR_OFF=GRAY)
-display2.init_row(X_ORG=2, Y_ORG=25, COL_INTV=5)
+display2.init_row(X_ORG=1, Y_ORG=25, COL_INTV=5)
 
 display3 = Seven_seg(screen)
 display3.init_col(BLOCK_SIZE=9, BLOCK_INTV=9, COLOR_ON=(120, 200, 250), COLOR_OFF=GRAY)
