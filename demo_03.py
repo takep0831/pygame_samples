5499f45989446580fc07f05d513ea3362d72dabb
takep0831 <195740480+takep0831@users.noreply.github.com>
2025-06-14 13:54:08 +0900

    a

--- demo_03.py
+++ demo_03.py
@@ -48,11 +48,10 @@ while running:
         # 「for count」のループから抜ける。whileループも抜ける。
         
         dt_now = datetime.now()
-        lcd1 = LCD_10
         time_now = (dt_now.hour * 10 ** 6
                     + dt_now.minute * 10 ** 3
                     + dt_now.second * 1
-                    + lcd1.update_col(col=10, code=10) * 100)
+                    + LCD_10 * 100)
 
         display1.disp_num2(zfil=True, rjust=8, num=time_now, base=10)
 
